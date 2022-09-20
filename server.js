4adb4878d20f2d7dee09c7e5365ec0bd96eb32a1
adamdasovich <adamdasovich@gmail.com>
2022-09-20 09:03:20 -0400

    added path package

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+import path from 'path'
 
 dotenv.config()
 connectDB();
